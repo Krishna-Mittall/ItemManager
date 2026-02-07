3f432d2aa5c21886fecdc91d05dc97c0fdc21ff2
Krishna Mittal <krishnamittal9691@gmail.com>
2026-02-07 15:09:43 +0530

    Update ItemController.java

--- src/main/java/org/cfs/smarttaskmanager/Controller/ItemController.java
+++ src/main/java/org/cfs/smarttaskmanager/Controller/ItemController.java
@@ -6,6 +6,7 @@ import org.cfs.smarttaskmanager.Service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("item")
 public class ItemController {
